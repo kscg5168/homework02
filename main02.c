ffa041bdb9026852437550a5eb7a36a6dea823c3
kscg5168 <gorden88115@gmail.com>
2018-06-01 09:20:51 +0800

    指向常數資料的非常數指標

---
+++ main02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(const int* ,int *,int);
+
+int main() {
+    int array[10]={9,4,8,7,94,87,13,15,5,1};
+    int i=0;
+    printf("Data items in original order:");
+    
+     for( i = 0; i < 10; i++ ) {
+        printf("%3d", array[i]);
+    }
+    bubbleSort(array,array,10);
+    printf("\n\nData items in ascending order:");
+    for( i = 0; i < 10; i++ ) {
+        printf("%3d", array[i]);
+    }
+    printf("\n\n");
+  system("PAUSE");	
+  return 0;
+}
